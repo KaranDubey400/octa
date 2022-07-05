907014f7d920da6ec44ecefc1eacc45a288ff853
octa elliptical <95653930+OctaElliptical@users.noreply.github.com>
2022-07-06 05:09:40 +0530

    Add files via upload

--- javascriptfinal.js
+++ javascriptfinal.js
@@ -1,51 +1,90 @@
+
+
+
+
+
+function pika(){
+    var fname = document.getElementById('school').value;
+    var lname = document.getElementById('lname').value; 
+    var fullusername = fname + " " + lname ;
+    var email = document.getElementById('chico').value;
+    var phone = document.getElementById('pokemon').value;
+    var country = document.getElementById('country').value;
+  
+    
+    document.getElementById("spiderman").innerHTML =  "<h1>order summary</h1>";
+    document.getElementById('spiderman').innerHTML += "<p>" + fullusername + "</p>";
+    document.getElementById('spiderman').innerHTML +=  email + "<br>" + phone +"<br>"+ country ;
+  
    
+  }
  
+    function phone1() {
+      var fphone = document.getElementById('pokemon').value;
+      if(fphone.length != 10 ){
+        document.getElementById("doremon").innerHTML = "*phone number must be of 10 digits";
+        document.getElementById("doremon").style.color = "red";
 
+      }
+      
+      else
+      document.getElementById("doremon").innerHTML = " ";
+    }
 
-    errors = document.getElementsByClassName('formerror');
-    for(let item of errors)
-    {
-        item.innerHTML = "";
+    function email1() {
+
+        var femail =  document.getElementById('chico').value;
+      if(femail.length >18){
+        document.getElementById('thor').innerHTML = "Length upto 18 letters allowed";
+        document.getElementById('thor').style.color= "red";
+  
+      }
+      else 
+      document.getElementById('thor').innerHTML = " ";
+
     }
 
 
-}
-function seterror(id, error){
-    //sets error inside tag of id 
-    element = document.getElementById(id);
-    element.getElementsByClassName('formerror')[0].innerHTML = error;
-   
-function validateForm(){
-    var returnval = true;
-    clearErrors();
-	    //performing the validation if validation fails then returnig the value false
-   
-   
-   var name = document.forms['myForm']["fname"].value;
-    if (name.length<5){
-        seterror("name", "*Length of name is too short");
-        returnval = false;
+    function totalorder() {
+      var pizza = document.getElementById("price1").value;
+      var qtn1 = document.getElementById("evior").value;
+      
+     
+
+      var pasta = document.getElementById("price2").value;
+      var qtn2 = document.getElementById("italianpasta").value;
+      var orderpizza = pizza * qtn1 + pasta * qtn2 ;
+     
+
+      var burger = document.getElementById("price3").value;
+      var qtn3 = document.getElementById("kingburger").value;
+      var orderburger = burger * qtn3 ;
+
+      var NOODLES = document.getElementById("price4").value;
+      var qtn4 = document.getElementById("chinesenoodles").value;
+      var ordernoodles = NOODLES * qtn4 ;
+
+      var waterbottle = document.getElementById("price5").value;
+      var qtn5 = document.getElementById("waterbottle1").value;
+      var orderwaterbottle = waterbottle * qtn5 ;
+    
+      
+      document.getElementById("lexi").innerHTML = orderpizza +"dollars";
+      document.getElementById("macaroni").innerHTML += orderpasta;
+      document.getElementById("kinggburger").innerHTML += orderburger;
+      document.getElementById("shezwannoodles").innerHTML += ordernoodles;
+      document.getElementById("mineralwaterbottle").innerHTML += orderwaterbottle + "dollars$";
+
+
+
     }
 
 
+  
+  
 
-    var email = document.forms['myForm']["femail"].value;
-    if (email.length>15){
-        seterror("email", "*Email length is too long");
-        returnval = false;
-    }
 
-    var phone = document.forms['myForm']["fphone"].value;
-    if (phone.length != 10){
-        seterror("phone", "*Phone number should be of 10 digits!");
-        returnval = false;
-   var phone = document.forms['myForm']["fphone"].value;
-   
+  
 
-    if (phone.length != 10){
-        seterror("phone", "*Phone number should be of 10 digits!");
-        returnval = false;
-    }
-	 return returnval;
+    
 
-}
